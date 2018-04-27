f371b20b39a9a2db830869666c1c051aa85c0808
Mikhail Baikov <mikhail.baikov@gmail.com>
2018-04-28 01:34:51 +0300

    up refs

--- CSC/reference.cpp
+++ CSC/reference.cpp
@@ -3,11 +3,13 @@
 int main(){
 
 int x;
-int& rx = x; // from now on, rx is just like x.
-// Unlike pointers, refs are not real objects in memory.
-int* p = &x; // Ok
-int* pr = &rx; // OK! but remember that rx is just x!
-// i.e. rx is not something that exists alone, it has to refer to something else.
+int& rx = x;
+
+x = 10;
+
+int* p = &x;
+int* pr = &rx;
+
 printf ("%d %d %p %p\n",x,rx,p,pr);
 return 0;
 }
