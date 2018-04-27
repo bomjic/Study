ae460a510b5986d462abb4fa4a232dbddc1b475f
Mikhail Baikov <mikhail.baikov@gmail.com>
2018-04-28 00:53:55 +0300

    pointers update

--- CSC/pointers.c
+++ CSC/pointers.c
@@ -1,10 +1,14 @@
 #include <stdio.h> 
 int main(){ 
 	int i[10];
+	i[1]=254;
 	char c[10]; 
 	int *pi = &i[1]; 
 	char *pc = &c[0]; 
 	printf("i %d\n", i[1]); 
 	printf("pi %p\n",pi);
+	int *p1 = i;
+	int *p2 = &i[0];
+	printf("%p %p\n", p1, p2);
 	return 0; 
 }
