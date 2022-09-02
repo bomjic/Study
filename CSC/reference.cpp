49ea237aabe0dd8952eb9cff31f29e926cb6f989
Mikhail Baikov <mikhail.baikov@gmail.com>
2022-09-02 20:44:07 +0000

    clean

--- CSC/reference.cpp
+++ CSC/reference.cpp
@@ -7,10 +7,12 @@ int& rx = x;
 
 x = 10;
 
+printf("test\n");
+
 int* p = &x;
 int* pr = &rx;
 
-printf ("x\trx\tp\tpr");
+printf ("x rx p pr \n");
 printf ("%d %d %p %p\n",x,rx,p,pr);
 return 0;
 }
--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 cd CSC;
-make
+make clean;
+make pointers;
+make reference;
+
